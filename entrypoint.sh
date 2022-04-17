f9529e7fa2e65cd0670c6d82073fa05ca162e4de
Tal Tchernihovski <taltcher@gmail.com>
2022-04-17 15:05:33 +0300

    Fix json format command for new ember template lint version

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 
 "$(npm bin)"/ember-template-lint --version
 
-DISABLE_GITHUB_ACTIONS_ANNOTATIONS=true "$(npm bin)"/ember-template-lint --json ${INPUT_TEMPLATE_LINT_FLAGS:-'.'} > /temp.json
+DISABLE_GITHUB_ACTIONS_ANNOTATIONS=true "$(npm bin)"/ember-template-lint --format=json "${INPUT_TEMPLATE_LINT_FLAGS:-'.'}" > /temp.json
 
 cat /temp.json | node /formatter.js > /temp_rdjson.json
 
